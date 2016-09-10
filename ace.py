c7dc77a0572af9a64dbb3d11a3989eb34a0a6432
omelyanchikd <omelyanchikd@ukr.net>
2016-09-10 20:35:13 +0200

    Added input files with data to use

--- ace.py
+++ ace.py
@@ -2,10 +2,10 @@ import pandas
 
 #datafile = "C:\Diana\Poland\Poland\poland_aggregate.csv"
 #datafile = "C:/Users/d.omelianchyk/Downloads/poland.csv"
-datafile = "D:\Poland\poland.csv"
+datafile = "poland.csv"
 history = pandas.read_csv(datafile, sep = ";", decimal = ",")
 
-datafile = "D:\Poland\poland_info.csv"
+datafile = "poland_info.csv"
 #datafile = "D:\Poland\poland_info_copy.csv"
 #datafile = "C:/Users/d.omelianchyk/Downloads/poland_info.csv"
 firm_info = pandas.read_csv(datafile, sep = ";", decimal = ",")
@@ -16,13 +16,14 @@ print(firm_info)
 
 from sklearn import linear_model
 
-#clf = linear_model.BayesianRidge(compute_score= True)
-clf = linear_model.LinearRegression()
+clf = linear_model.BayesianRidge(compute_score= True)
+#clf = linear_model.LinearRegression()
 clf.fit(history[['workers', 'subsidies']], history['sales'])
 
 print(clf.score(history[['workers', 'subsidies']], history['sales']))
 
 from world import World
+from sklearn.metrics import r2_score
 
 poland = World(history['employees'][0], firm_info, history)
 
@@ -30,19 +31,30 @@ steps = 34
 
 sales_file = open("sales.txt", "w")
 workers_file = open("workers.txt", "w")
-
-for step in range(steps):
-    poland.step(history['subsidies'][step], history['employees'][step])
-    print("Step " + str(step + 1) + " finished." )
-    sales_file.write("%.2f" % poland.sales[step]+ '\n')
-    workers_file.write(str(poland.workers[step]) + '\n')
-
+rscore_file = open("rscore.txt", "w")
 sales_file.close()
 workers_file.close()
+rscore_file.close()
 
-from sklearn.metrics import r2_score
+for step in range(steps):
+    sales_file = open("sales.txt", "a")
+    workers_file = open("workers.txt", "a")
+    rscore_file = open("rscore.txt", "a")
+    poland.step(history['subsidies'][step], history['employees'][step])
+    print("Step " + str(step + 1) + " finished." )
+    rscore = r2_score(history['revenues'][:len(poland.sales)], poland.sales)
+    print(rscore)
+    sales_file.write("%.2f" % poland.sales[step]+ '\n')
+    workers_file.write(str(poland.workers[step]) + '\n')
+    rscore_file.write("%.4f" % rscore + '\n')
+    sales_file.close()
+    workers_file.close()
+    rscore_file.close()
 
-print(r2_score(history['revenues'][:len(poland.sales)], poland.sales))
+
+
+
+
 
 import matplotlib.pyplot as plt
 
